243db1c32dcfcd5c2c9444473e56821d721ea9af
Chris Boylen <billpolster@gmail.com>
2018-11-06 10:54:36 -0700

    Update Card snapshot test

--- src/__tests__/Card.test.js
+++ src/__tests__/Card.test.js
@@ -7,26 +7,26 @@ describe('Card', () => {
   let wrapper;
 
   beforeEach(() => {
-    mockCard = [{
-          "id": 142035,
-          "technique": "Drypoint and aquatint",
-          "imagepermissionlevel": 0,
-          "primaryimageurl": "https://nrs.harvard.edu/urn-3:HUAM:INV166572_dynmc",
-          "people": [{
-            "alphasort": "Hewitt, Charles",
-            "birthplace": "Lewiston, Maine",
-            "personid": 22803,
-            "prefix": null,
-            "name": "Charles Hewitt",
-            "role": "Artist",
-            "gender": "unknown",
-            "displayorder": 1,
-            "displaydate": "born 1946",
-            "culture": "American",
-            "displayname": "Charles Hewitt",
-            "deathplace": null
-          }]
-        }]
+    mockCard = {
+      "id": 142035,
+      "technique": "Drypoint and aquatint",
+      "imagepermissionlevel": 0,
+      "primaryimageurl": "https://nrs.harvard.edu/urn-3:HUAM:INV166572_dynmc",
+      "people": [{
+        "alphasort": "Hewitt, Charles",
+        "birthplace": "Lewiston, Maine",
+        "personid": 22803,
+        "prefix": null,
+        "name": "Charles Hewitt",
+        "role": "Artist",
+        "gender": "unknown",
+        "displayorder": 1,
+        "displaydate": "born 1946",
+        "culture": "American",
+        "displayname": "Charles Hewitt",
+        "deathplace": null
+      }]
+    }
     wrapper = mount(Card,{
       propsData: {
         card: mockCard
