66bd550743ad060b61ba60753b77aeec511b4fad
Chris Boylen <billpolster@gmail.com>
2018-11-06 18:21:12 -0700

    Server

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 app.use(cors());
 app.set('port', process.env.PORT || 3010);
+app.get('/', (request, response) => {
+  response.send('Hola!');
+});
 
 app.listen(app.get('port'));
